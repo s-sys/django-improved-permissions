6c92f5e58e983d2f63362ddf1702f90e4ee80876
Gabriel de Biasi <biasi131@gmail.com>
2018-03-06 18:36:29 -0400

    ver 0.0.14

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-improved-permissions',
-    version='0.0.13',
+    version='0.0.14',
     packages=[
         'improved_permissions',
         'improved_permissions.migrations',
