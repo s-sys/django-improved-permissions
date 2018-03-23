9898a2ce0abb450719c3236432be8f005d73baf7
Gabriel de Biasi <biasi131@gmail.com>
2018-03-23 16:02:32 -0400

    Refactor getters

--- improved_permissions/shortcuts.py
+++ improved_permissions/shortcuts.py
@@ -1,10 +1,10 @@
 """ permissions shortcuts """
 # pylint: disable=too-many-lines
-from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 
+from improved_permissions import getters
 from improved_permissions.exceptions import (InvalidPermissionAssignment,
-                                             InvalidRoleAssignment, NotAllowed)
+                                             InvalidRoleAssignment)
 from improved_permissions.models import RolePermission, UserRole
 from improved_permissions.roles import ALL_MODELS
 from improved_permissions.utils import (check_my_model, delete_from_cache,
@@ -15,141 +15,15 @@ from improved_permissions.utils import (check_my_model, delete_from_cache,
 
 
 def get_user(role_class=None, obj=None):
-    """
-    Get the User instance attached to the object.
-    Only one UserRole must exists and this relation
-    must be unique=True.
-
-    Returns None if there is no user attached
-    to the object.
-    """
-    query = UserRole.objects.select_related('user').all()
-    role = None
-
-    if role_class:
-        # All users who have "role_class" attached to any object.
-        role = get_roleclass(role_class)
-        query = query.filter(role_class=role.get_class_name())
-
-    if obj:
-        # All users who have any role attached to the object.
-        ct_obj = ContentType.objects.get_for_model(obj)
-        query = query.filter(content_type=ct_obj.id, object_id=obj.id)
-
-    # Check if object belongs
-    # to the role class.
-    check_my_model(role, obj)
-
-    # Looking for a role class using unique=True
-    selected = list()
-    for ur_obj in query:
-        role = get_roleclass(ur_obj.role_class)
-        if role.unique is True:
-            selected.append(ur_obj.user)
-
-    users_set = set(selected)
-    if len(users_set) > 1:
-        raise NotAllowed(
-            'Multiple unique roles was found using '
-            'the function get_user.  Use get_users '
-            'instead.'
-        )
-    if len(users_set) == 1:
-        return selected[0]
-    return None
+    return getters.get_user(role_class, obj)
 
 
 def get_users(role_class=None, obj=None):
-    """
-    If "role_class" and "obj" is provided,
-    returns a QuerySet of users who has
-    this role class attached to the
-    object.
-
-    If only "role_class" is provided, returns
-    a QuerySet of users who has this role
-    class attached to any object.
-
-    If neither "role_class" or "obj" are provided,
-    returns all users of the project.
-    """
-
-    query = get_user_model().objects.all()
-    role = None
-
-    if role_class:
-        # All users who have "role_class" attached to any object.
-        role = get_roleclass(role_class)
-        query = query.filter(roles__role_class=role.get_class_name())
-
-    if obj:
-        # All users who have any role attached to the object.
-        ct_obj = ContentType.objects.get_for_model(obj)
-        query = query.filter(roles__content_type=ct_obj.id, roles__object_id=obj.id)
-
-    # Check if object belongs
-    # to the role class.
-    check_my_model(role, obj)
-
-    # Return as a distinct QuerySet.
-    return query.distinct()
+    return getters.get_users(role_class, obj)
 
 
 def get_objects(user, role_class=None, model=None):
-    """
-    Return the list of objects attached
-    to a given user.
-
-    If "role_class" is provided, only the objects
-    which as registered in that role class will
-    be returned.
-
-    If "model" is provided, only the objects
-    of that model will be returned.
-    """
-    query = UserRole.objects.filter(user=user)
-    role = None
-
-    if role_class:
-        # Filtering by role class.
-        role = get_roleclass(role_class)
-        query = query.filter(role_class=role.get_class_name())
-
-    if model:
-        # Filtering by model.
-        ct_obj = ContentType.objects.get_for_model(model)
-        query = query.filter(content_type=ct_obj.id)
-
-    # Check if object belongs
-    # to the role class.
-    check_my_model(role, model)
-
-    # TODO
-    result = set(ur_obj.obj for ur_obj in query)
-    # TODO
-    return list(result)
-
-
-def get_permissions(user, role_class, obj=None):
-    """
-    Return the list of permissions attached
-    to a given user and a role.
-    """
-    role = get_roleclass(role_class)
-    query = (UserRole.objects
-             .prefetch_related('accesses')
-             .filter(role_class=role.get_class_name())
-             .filter(user=user))
-
-    if obj and role.models == ALL_MODELS or not obj and role.models != ALL_MODELS:
-        raise NotAllowed()
-    elif obj and role.is_my_model(obj):
-        ct_obj = ContentType.objects.get_for_model(obj)
-        ur_obj = query.get(content_type=ct_obj.id, object_id=obj.id)
-    else:
-        ur_obj = query.get(content_type__isnull=True, object_id__isnull=True)
-
-    return ur_obj.accesses.all()
+    return getters.get_objects(user, role_class, model)
 
 
 def has_role(user, role_class=None, obj=None):
