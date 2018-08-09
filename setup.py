b5e568ae5e284510244ab67b9030e337b2481e0b
Gabriel de Biasi <biasi131@gmail.com>
2018-08-09 15:28:54 -0400

    update version to 0.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-improved-permissions',
-    version='0.2.1',
+    version='0.2.2',
     packages=[
         'improved_permissions',
         'improved_permissions.migrations',
